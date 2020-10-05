565f2497e083120d478d04b9e38e5079b2569cf4
keving21 <galani.kevin@gmail.com>
2020-10-04 22:31:02 -0400

    Update PathSum.java

--- PathSum.java
+++ PathSum.java
@@ -1,38 +1,34 @@
-public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+ 
 class Solution {
     public boolean hasPathSum(TreeNode root, int sum) {
-
-        if (root ==null) return false;
-        if(root != null && root.left == null && root.right == null && root.val != sum) return false;
-
+        
+     
         return check(root, sum, 0, false);
-
-
+        
+        
     }
-
+    
     public boolean check (TreeNode temp, int sum, int value, boolean match){
         if(temp != null){
-
+         
             value += temp.val;
             match = check(temp.left, sum, value, match);
             match = check(temp.right, sum, value, match);
-            if(temp.left == null && temp.right == null && sum == value) {
-                return true;
-            }
+            if(temp.left == null && temp.right == null && sum == value) return true;
         }
         return match;
     }
-
+    
 }
