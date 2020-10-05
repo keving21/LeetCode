75c0a6c5f33e7820254217ff78e09c52f3fbfe4e
keving21 <galani.kevin@gmail.com>
2020-10-04 22:32:17 -0400

    Update PathSum.java

--- PathSum.java
+++ PathSum.java
@@ -13,16 +13,11 @@
  
 class Solution {
     public boolean hasPathSum(TreeNode root, int sum) {
-        
-     
         return check(root, sum, 0, false);
-        
-        
     }
     
     public boolean check (TreeNode temp, int sum, int value, boolean match){
         if(temp != null){
-         
             value += temp.val;
             match = check(temp.left, sum, value, match);
             match = check(temp.right, sum, value, match);
@@ -30,5 +25,4 @@ class Solution {
         }
         return match;
     }
-    
 }
